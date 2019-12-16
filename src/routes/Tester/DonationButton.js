2406767644ecbbafeeefc69aca35e91c4bab5531
Raphael Beer <raphael.beer@protonmail.com>
2019-12-16 20:06:12 +0100

    Add: DonationButton

---
+++ src/routes/Tester/DonationButton.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export default () => {
+  return (
+    <button className="uppercase">support us</button>
+  );
+}
--- src/routes/Tester/Title.js
+++ src/routes/Tester/Title.js
@@ -2,9 +2,16 @@ import React from 'react';
 
 export default () => {
   return (
-    <div className="mt-10 mb-6 text-center">
-      <h2 className="w-screen font-lobster leading-title text-title text-twitterblue">
-        Is <span className="text-accent-purple">@username</span><br /> shadowbanned on Twitter?</h2>
-    </div>
+    <h2 className="
+      w-screen
+      mt-10 mb-6
+      font-lobster
+      leading-title
+      text-title
+      text-twitterblue
+    ">
+      Is <span className="text-accent-purple">@username</span><br />
+      shadowbanned on Twitter?
+    </h2>
   );
 }
