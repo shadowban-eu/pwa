fe8dd898712c75fa4d9e773576f076029b8dd54e
Raphael Beer <raphael.beer@protonmail.com>
2019-12-22 16:26:11 +0100

    Add: Footer

--- src/App.js
+++ src/App.js
@@ -2,13 +2,17 @@ import React from 'react';
 import { Router } from '@reach/router';
 
 import Tester from './routes/Tester';
+import Footer from './Footer';
 
 function App() {
   return (
-    <Router>
-      <Tester path="/" default/>
-      <Tester path="/:screenName" />
-    </Router>
+    <div>
+      <Router>
+        <Tester path="/" default/>
+        <Tester path="/:screenName" />
+      </Router>
+      <Footer />
+    </div>
   );
 }
 
---
+++ src/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <div className="flex py-8 bg-shadowblue justify-center">
+      <div className="text-xs">
+        Made in Germany by <a href="https://twitter.com/shadowban_eu">@shadowban_eu</a>
+      </div>
+    </div>
+  );
+}
+
+export default Footer;
