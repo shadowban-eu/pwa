e238a43b21204950038a712219bbcb4158dd6b43
Raphael Beer <raphael.beer@protonmail.com>
2019-12-27 13:46:19 +0100

    Add: SWR hook to fetch test results from URL

--- src/routes/Tester.js
+++ src/routes/Tester.js
@@ -1,8 +1,14 @@
 import React from 'react';
 import { createStore, useStore } from 'react-hookstore';
+import useSWR from 'swr';
 
 import { initialState, reducer } from '../reducers/tester';
-import { SET_SCREEN_NAME, VALIDATE_SCREEN_NAME } from '../actions';
+import {
+  SET_SCREEN_NAME,
+  VALIDATE_SCREEN_NAME,
+  RUN_TEST,
+  SET_CURRENT_RESULTS
+} from '../actions';
 
 import Title from './Tester/Title';
 import DonationButton from './Tester/DonationButton';
@@ -11,9 +17,27 @@ import Results from './Tester/Results';
 
 createStore('tester', initialState, reducer);
 
+// const fetchAndDispatchResults = async (...args) => {
+//   const res = await fetch(...args);
+//   const results
+// };
+
 const Tester = (props) => {
   const { screenName } = props;
-  const [, dispatch] = useStore('tester');
+  const [{ valid }, dispatch] = useStore('tester');
+  useSWR(
+    screenName && valid ? `${process.env.REACT_APP_TEST_URL}/${screenName}` : null,
+    {
+      fetcher: async (...args) => {
+        dispatch({ type: RUN_TEST });
+        const res = await fetch(...args);
+        const result = await res.json();
+        dispatch({ type: SET_CURRENT_RESULTS, result });
+      },
+      revalidateOnFocus: false,
+      shouldRetryOnError: false
+    }
+  );
 
   React.useEffect(() => {
     if (screenName) {
@@ -21,7 +45,6 @@ const Tester = (props) => {
       dispatch({ type: VALIDATE_SCREEN_NAME, screenName });
     }
   }, [screenName, dispatch]);
-
 
   return (
     <div className="flex flex-col bg-shadowblue">
