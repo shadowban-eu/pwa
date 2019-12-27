938ed9d299922c75e01c202dbf3b173f9f7d7dd2
Raphael Beer <raphael.beer@protonmail.com>
2019-12-27 13:57:36 +0100

    Change: navigate on 'check' click, instead of fetching +
      Since changes to /{screenName} in the URL
      trigger a test run, submitting user input
      navigates to /{'tester'Store.screenName}

--- src/routes/Tester/Controls.js
+++ src/routes/Tester/Controls.js
@@ -1,19 +1,15 @@
 import React from 'react';
+import { navigate } from '@reach/router';
 import { useStore } from 'react-hookstore';
 
-import { RUN_TEST, SET_CURRENT_RESULTS } from '../../actions';
 import ScreenNameInput from './ScreenNameInput';
 
-
 const Controls = () => {
-  const [{ screenName }, dispatch] = useStore('tester');
+  const [{ screenName }] = useStore('tester');
 
   const runTest = async (submitEvent) => {
     submitEvent.preventDefault();
-    dispatch({ type: RUN_TEST });
-    const res = await fetch(`${process.env.REACT_APP_TEST_URL}/${screenName}`);
-    const result = await res.json();
-    dispatch({ type: SET_CURRENT_RESULTS, result });
+    navigate(`/${screenName}`);
   };
 
   return (
