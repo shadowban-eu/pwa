1fb4944257097a11d338bc6a22ea9c12abaea48d
Raphael Beer <raphael.beer@protonmail.com>
2020-01-04 18:40:28 +0100

    Add: modal in-animation

--- src/DonateModal.js
+++ src/DonateModal.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { useTranslation } from 'react-i18next';
 import useModal from 'use-react-modal';
+import { useTransition, animated } from 'react-spring';
 
 import BBText from './BBText';
 
@@ -9,6 +10,17 @@ const DonateModal = () => {
   const { isOpen, openModal, closeModal, Modal } = useModal({
     background: 'rgba(0, 0, 0, 0.5)'
   });
+  const transitions = useTransition(isOpen, null, {
+    from: {
+      transform: 'scale(0)'
+    },
+    enter: {
+      transform: 'scale(1)'
+    },
+    leave: {
+      transform: 'scale(0)'
+    }
+  })
 
   const handleDonateClick = (evt) => {
     window.open('https://www.paypal.me/shadowban');
@@ -21,29 +33,35 @@ const DonateModal = () => {
         support us
       </button>
       {
-        isOpen ?
-          <Modal
-            className="
-              card
-              flex flex-col justify-around
-              sm:w-11/12 md:w-2/3
-              h-modal
-              overflow-auto
-            "
-          >
-            <BBText>
-              { t('donateModal.content') }
-            </BBText>
-            <div className="flex flex-row justify-end">
-              <button className="mr-4" onClick={handleDonateClick}>
-                { t('donateModal.donateButton')}
-              </button>
-              <button onClick={closeModal}>
-                { t('donateModal.dismissButton') }
-              </button>
-            </div>
-          </Modal>
-          : null
+        isOpen ? <Modal className="
+          sm:w-11/12 md:w-2/3
+          h-modal
+          overflow-auto
+        ">
+          {
+            transitions.map(({ item, props, key }) => (
+              item ?
+                <animated.div key={key} style={props} className="
+                  card
+                  flex flex-col justify-around
+                ">
+                <BBText>
+                  { t('donateModal.content') }
+                </BBText>
+                <div className="flex flex-row justify-end">
+                  <button className="mr-4" onClick={handleDonateClick}>
+                    { t('donateModal.donateButton')}
+                  </button>
+                  <button onClick={closeModal}>
+                    { t('donateModal.dismissButton') }
+                  </button>
+                </div>
+              </animated.div>
+              : null
+            ))
+          }
+        </Modal>
+        : null
       }
     </>
   );
