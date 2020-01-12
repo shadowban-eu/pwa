df85becc79058df9ec1e0c8a2312df094a0b2a52
Raphael Beer <raphael.beer@protonmail.com>
2020-01-12 16:36:02 +0100

    Add: links to subject tweet and its author in Title

--- src/routes/Resurrect/Title.js
+++ src/routes/Resurrect/Title.js
@@ -2,9 +2,23 @@ import React from 'react';
 import { useStore } from 'react-hookstore';
 import { useTranslation } from 'react-i18next';
 
+import SafeLink from '../../SafeLink';
+
 const Title = () => {
-  const [{ title, fetching }] = useStore('resurrect');
+  const [{ result, title, fetching }] = useStore('resurrect');
   const { t } = useTranslation('resurrect');
+
+  let subjectTweetLink;
+  let subjectAuthorLink;
+  if (result) {
+    const { parentId } = result.tweets.testedWith;
+    const { parentAuthorScreenName } = result.tweets.testedWith;
+    subjectTweetLink = `https://twitter.com/shadowban_eu/status/${parentId}`;
+
+    if (result.protected || result.suspended) {
+      subjectAuthorLink = `https://twitter.com/${parentAuthorScreenName}`;
+    }
+  }
 
   return (
     <h2 className="
@@ -15,11 +29,25 @@ const Title = () => {
       text-smtitle sm:text-smtitle md:text-title
       text-twitterblue
     ">
-      This tweet is <span className="text-accent-purple">
+      {
+        subjectTweetLink
+          ? <SafeLink href={subjectTweetLink} icon>This tweet</SafeLink>
+          : 'This tweet'
+      }
+      { ' is ' }
+      <span className="text-accent-purple">
       {
         fetching
           ? t('title.fetching')
-          : t(title)
+          : subjectAuthorLink
+            ? <SafeLink
+                href={subjectAuthorLink}
+                className="text-accent-purple"
+                icon
+                >
+                {t(title)}
+              </SafeLink>
+            : t(title)
       }
       </span>
     </h2>
