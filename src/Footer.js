425883e7c6347373a67770ade1568c23cde0d80e
Raphael Beer <raphael.beer@protonmail.com>
2021-05-04 10:17:16 +0200

    Change: footer hash on own line

--- src/Footer.js
+++ src/Footer.js
@@ -4,16 +4,16 @@ import SafeLink from './SafeLink';
 
 const Footer = () => {
   const commitHref = `https://github.com/shadowban-eu/pwa/commit/${process.env.GIT_COMMIT}`;
+  const commitAlt = `${process.env.GIT_BRANCH}@${process.env.GIT_COMMIT}`;
   return (
     <div className="h-20 w-full py-8 font-normal text-center text-xs bg-shadowblue">
       Made in Germany by{' '}
       <SafeLink href="https://twitter.com/shadowban_eu">@shadowban_eu</SafeLink>
+      {' | '}
+      <SafeLink href="https://github.com/shadowban-eu">GitHub</SafeLink>
       <br />
-      <SafeLink href="https://github.com/shadowban-eu">GitHub</SafeLink>{' '}
       <SafeLink href={commitHref}>
-        <span className="font-hairline">
-          ({process.env.GIT_BRANCH}@{process.env.GIT_COMMIT})
-        </span>
+        <span className="font-hairline">{commitAlt}</span>
       </SafeLink>
     </div>
   );
