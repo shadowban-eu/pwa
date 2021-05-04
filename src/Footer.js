e51b3287864e883d1daa9c0802c79800682ac5ae
Raphael Beer <raphael.beer@protonmail.com>
2021-05-04 10:03:35 +0200

    Fix: Footer not using SafeLinks

--- src/Footer.js
+++ src/Footer.js
@@ -7,9 +7,9 @@ const Footer = () => {
   return (
     <div className="h-20 w-full py-8 font-normal text-center text-xs bg-shadowblue">
       Made in Germany by{' '}
-      <a href="https://twitter.com/shadowban_eu">@shadowban_eu</a>
+      <SafeLink href="https://twitter.com/shadowban_eu">@shadowban_eu</SafeLink>
       <br />
-      <a href="https://github.com/shadowban-eu">GitHub</a>{' '}
+      <SafeLink href="https://github.com/shadowban-eu">GitHub</SafeLink>{' '}
       <SafeLink href={commitHref}>
         <span className="font-hairline">
           ({process.env.GIT_BRANCH}@{process.env.GIT_COMMIT})
